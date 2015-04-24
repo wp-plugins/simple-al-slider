a657af4002dcbf57df1201445033c129fb557771
alexdtn <alexdtn@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-04-24 12:22:18 +0000

    tagging version 1.2.9
    git-svn-id: https://plugins.svn.wordpress.org/simple-al-slider/trunk@1144636 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- simple-al-slider.php
+++ simple-al-slider.php
@@ -2,7 +2,7 @@
 /*
 Plugin Name: Simple Al Slider
 Description: Simple slider with background and elements. And capability to add images, buttons, html templates and videos(youtube...).
-Version: 1.2.8
+Version: 1.2.9
 Author: alexdtn (<a href="http://al-plugins.biz/">homesite</a>)
 */
 
